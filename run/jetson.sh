bebc924fd595c6c462967c1d641305101c4f69c9
MarcusHsieh <marcus.j.hsieh@gmail.com>
2025-04-06 08:42:20 -0700

    Update run script

--- run/jetson.sh
+++ run/jetson.sh
@@ -7,7 +7,7 @@ set -e
 echo "--- Setting up variables for Docker ---"
 
 # Mount host MOLL-E folder into container
-HOST_MOLLE_PATH=../../MOLL-E
+HOST_MOLLE_PATH=..
 CONTAINER_MOLLE_PATH=/MOLL-E
 
 # --- Configuration ---
